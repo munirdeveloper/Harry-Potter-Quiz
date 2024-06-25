2755005ab53167143a163f30dfa18dfd0d61802d
Munir <munircsdev@gmail.com>
2024-06-25 11:36:20 +0300

    vite configs

---
+++ hp-app/.vercel/output/static/_app/immutable/entry/start.nTB4U6aM.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BdRrpuP3.js";export{t as start};
--- hp-app/vite.config.ts
+++ hp-app/vite.config.ts
@@ -1,8 +1,20 @@
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
-import { sveltekit } from '@sveltejs/kit/vite';
+// import { purgeCss } from 'vite-plugin-tailwind-purgecss';
+// import { sveltekit } from '@sveltejs/kit/vite';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+// 	base: "/Harry-Potter-Quiz/",
+// 	plugins: [sveltekit(), purgeCss()]
+// });
+
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 export default defineConfig({
-	base: "/Harry-Potter-Quiz/",
-	plugins: [sveltekit(), purgeCss()]
+  base: "/Harry-Potter-Quiz/", // Specify the base URL for your project (optional)
+  plugins: [
+    sveltekit(), // Use SvelteKit Vite plugin
+    purgeCss()   // Use PurgeCSS plugin for Tailwind CSS
+  ]
 });
